6e8ce83cb4b2a8277d491f1292a3264246587d1a
mehedi <mehedi25122003@gmail.com>
2025-01-18 19:03:23 +0600

    learing something

--- part2.js
+++ part2.js
@@ -14,3 +14,11 @@ const ultimatemax=getmax(max2,tmax);
 
 const foundi=Math.max(22,21);
 console.log(max2,tmax, foundi,ultimatemax);
+
+
+
+
+
+let ramdon=[1,22,34,56,7,8,8,6]
+const finding=ramdon.sort()
+console.log(finding)
