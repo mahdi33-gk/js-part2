b8cfdfc4ed137d06584ba7f8c04a4450e6ca7121
mehedi <mehedi25122003@gmail.com>
2025-01-18 19:58:00 +0600

    quantity code written

--- quantity.js
+++ quantity.js
@@ -14,3 +14,24 @@ function getpayableMoney(bedquantity,chairQuantity,tableQuantity){
 
 const needmoney= getpayableMoney(22,3,60);
 console.log(needmoney);
+
+
+
+
+
+function goodshop(fishquantity,fruitsquantity,vegetablesquantity){
+    const fishprice=400;
+    const fruitprice=3;
+    const vegetableprice=1;
+
+    const payforFish= fishquantity * fishprice;
+    const payforFruits= fruitsquantity * fruitprice;
+    const payforVegetables= vegetablesquantity * vegetableprice;
+
+    const totalMoney= payforFish + payforFruits + payforVegetables;
+
+
+    return totalMoney;
+}
+const havetopay= goodshop(2,5,12);
+console.log(havetopay);
