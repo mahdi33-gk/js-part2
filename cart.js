2198469d30784e69f99d13d068a738428a8aa941
mehedi <mehedi25122003@gmail.com>
2025-01-19 16:00:59 +0600

    skyrock

--- cart.js
+++ cart.js
@@ -14,3 +14,38 @@ function allmonmey(items){
 }
 const totalpayablemoney= allmonmey(shopping);
 console.log(totalpayablemoney);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const allproducts=[
+    { name:'pajabi',price:1100,quantity:2},
+    { name:'braash',price:100,quantity:3},
+    { name:'pant',price:500,quantity:4},
+    { name:'book',price:300,quantity:2},
+    { name:'shirt',price:200,quantity:1}
+]
+
+function getmoney(something){
+    let total=0;
+    for(const ites of something){
+        const actualPrice= ites.quantity * ites.price;
+        total=total+actualPrice;
+    }
+    return total;
+}
+const getmone= getmoney(allproducts);
+console.log(getmone);
