6e06f0659745c18e1641f8bcf1e0c7fb4fcb619a
mehedi <mehedi25122003@gmail.com>
2025-01-20 22:02:57 +0600

    done but not done actually

--- tasks2.js
+++ tasks2.js
@@ -1,24 +1,24 @@
 
-function Salary(empoyee){
-    let newSl=[];
-    for(const money of empoyee){
+// function Salary(empoyee){
+//     let newSl=[];
+//     for(const money of empoyee){
         
-        if(money.experience==0){
-            const newSalary= money.increment +money.starting;
-            newSl.push(newSalary);
-        }
-        else{
-            const newSalary= money.experience * money.increment;
-            const log= newSalary + money.starting;
-            newSl.push(log);
-        }
-    }
+//         if(money.experience==0){
+//             const newSalary= money.increment +money.starting;
+//             newSl.push(newSalary);
+//         }
+//         else{
+//             const newSalary= money.experience * money.increment;
+//             const log= newSalary + money.starting;
+//             newSl.push(log);
+//         }
+//     }
     
-    return newSl;
-}
+//     return newSl;
+// }
 
-const finalResult= Salary(employees);
-console.log(finalResult);
+// const finalResult= Salary(employees);
+// console.log(finalResult);
 
 const employees = [
     { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
@@ -26,9 +26,9 @@ const employees = [
     { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
     { name: "shohel", experience: 0, starting: 29000, increment: 4000 }
 ];
-
+let nowSalary=[];
 function all(persons){
-    let nowSalary=[];
+    
     for(const person of persons){
         if(person.experience !== 0){
             const salary=(person.experience * person.increment) + person.starting;
@@ -45,3 +45,13 @@ function all(persons){
  
 const reals=all(employees);
 console.log(reals);
+let i= 0;
+for(const per of nowSalary){
+    
+    i=i+per;
+    
+}
+console.log(i);
+
+const average= i/employees.length;
+console.log(average);
