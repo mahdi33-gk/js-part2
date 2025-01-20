acc6f8b29767600fd8bf767f9a4d204d1e8d9e75
mehedi <mehedi25122003@gmail.com>
2025-01-20 19:28:33 +0600

    al last

---
+++ tasks.js
@@ -0,0 +1,18 @@
+const mobiles=[
+    {name:'Iphone',brand:'apple',color:'black n white',price:100000},
+    {name:'Xiomi',brand:'redmi',color:'black n white',price:21000},
+    {name:'Walton',brand:'Bd',color:'black n white',price:10000},
+    {name:'Hv',brand:'bideshea',color:'black n white',price:12000},
+    {name:'SOny',brand:'Italy',color:'black n white',price:24000}
+]
+
+function priceAvrg(phones){
+    let dprice=0;
+    for(const phone of phones){
+        dprice= dprice + phone.price;
+    }
+    return dprice;
+}
+
+const avrg= priceAvrg(mobiles);
+console.log(avrg);
