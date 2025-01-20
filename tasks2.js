6179227351f63d2e20fe46d8f6a693e8e1016ca6
mehedi <mehedi25122003@gmail.com>
2025-01-20 21:27:07 +0600

    not found:

--- tasks2.js
+++ tasks2.js
@@ -2,16 +2,22 @@ const employees = [
     { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
     { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
     { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
-    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 }
 ];
-
-function newSalary(company){
-    for(const person of company){
-        Shahin_salary= person.experience * person.increment;
-        
-        console.log(Shahin_salary);
+function Salary(empoyee){
+    for(const money of empoyee){
+        if(money.experience==0){
+            const newSalary= money.increment +money.starting;
+            console.log(newSalary);
+        }
+        else{
+            const newSalary= money.experience * money.increment;
+            const log= newSalary + money.starting;
+            console.log(log);
+        }
     }
-
+    return 'shahin,shihab,shoikot,shohel.';
 }
-const allsalary= newSalary(employees);
-console.log(allsalary);
+
+const finalResult= Salary(employees);
+console.log(finalResult);
